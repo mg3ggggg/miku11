f06e6eac081512492486f95bad7a4eaeb9b0d314
mikuchan21 <138806631+mikuchan21@users.noreply.github.com>
2024-05-05 23:36:36 +0400

    Update حذف.js

--- plugins/حذف.js
+++ plugins/حذف.js
@@ -18,7 +18,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 };
 handler.help = ['del', 'delete'];
 handler.tags = ['group'];
-handler.command = /^حذف?$/i;
+handler.command = /^حذللللف?$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
@@ -33,7 +33,7 @@ conn.sendMessage(chat, { delete: m.quoted.vM.key })
 }
 handler.help = ['del', 'delete']
 handler.tags = ['tools']
-handler.command = /^حذف?$/i
+handler.command = /^حذلللللف?$/i
 handler.group = true
 handler.admin = true
 export default handler*/
