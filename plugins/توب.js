f2c70176a995243983a3e9fcd6a3e62cbe4b9847
mikuchan21 <138806631+mikuchan21@users.noreply.github.com>
2024-06-25 20:20:51 +0400

    Update توب.js

--- plugins/توب.js
+++ plugins/توب.js
@@ -1,23 +1,38 @@
 import util from 'util'
 import path from 'path'
+
 let user = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) throw `*مـثـال : .توب متفاعلين*`
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b = ps.getRandom()
-let c = ps.getRandom()
-let d = ps.getRandom()
-let e = ps.getRandom()
-let f = ps.getRandom()
-let g = ps.getRandom()
-let h = ps.getRandom()
-let i = ps.getRandom()
-let j = ps.getRandom()
-let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['✨','🤍','🔥','💫','😎', '💫', '🤍', '🤍', '✨', '🤍','✨','🤨','💫','🔥','🔥','💫', '🤍','🤍'])}`
-let l = Math.floor(Math.random() * x.length);
-let top = `*${x} توب 10 ${text} ${x}*
+let cooldowns = {}
+
+function handler(m, { groupMetadata, command, conn, text, usedPrefix }) {
+    let now = Date.now()
+    let groupId = m.chat
+
+    if (!cooldowns[groupId]) cooldowns[groupId] = {}
+    if (cooldowns[groupId][command] && now - cooldowns[groupId][command] < 20 * 60 * 1000) {
+        let remainingTime = ((20 * 60 * 1000) - (now - cooldowns[groupId][command])) / 1000
+        let minutes = Math.floor(remainingTime / 60)
+        let seconds = Math.floor(remainingTime % 60)
+        throw `*يمكنك استخدام هذا الأمر بعد ${minutes} دقيقة و ${seconds} ثانية*`
+    }
+
+    if (!text) throw `*مـثـال : .توب متفاعلين*`
+
+    let ps = groupMetadata.participants.map(v => v.id)
+    let a = ps.getRandom()
+    let b = ps.getRandom()
+    let c = ps.getRandom()
+    let d = ps.getRandom()
+    let e = ps.getRandom()
+    let f = ps.getRandom()
+    let g = ps.getRandom()
+    let h = ps.getRandom()
+    let i = ps.getRandom()
+    let j = ps.getRandom()
+    let k = Math.floor(Math.random() * 70)
+    let x = `${pickRandom(['✨', '🤍', '🔥', '💫', '😎', '💫', '🤍', '🤍', '✨', '🤍', '✨', '🤨', '💫', '🔥', '🔥', '💫', '🤍', '🤍'])}`
+    let l = Math.floor(Math.random() * x.length)
+    let top = `*${x} توب 10 ${text} ${x}*
 
 *1. ${user(a)}*
 *2. ${user(b)}*
@@ -29,11 +44,17 @@ let top = `*${x} توب 10 ${text} ${x}*
 *8. ${user(h)}*
 *9. ${user(i)}*
 *10. ${user(j)}*`.trim()
-conn.sendFile (m.reply (top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]}))}
+
+    conn.sendFile(m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j] }))
+    cooldowns[groupId][command] = now
+}
+
 handler.help = handler.command = ['توب']
 handler.tags = ['fun']
 handler.group = true
 handler.limit = 0
 export default handler
+
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+    return list[Math.floor(Math.random() * list.length)]
+}
