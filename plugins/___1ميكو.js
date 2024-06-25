19d1f60bdd878c0ef4196d2b0adcf9402eaf8b0f
mikuchan21 <138806631+mikuchan21@users.noreply.github.com>
2024-06-25 22:10:02 +0400

    Update ___1ميكو.js

--- plugins/___1ميكو.js
+++ plugins/___1ميكو.js
@@ -4,11 +4,11 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 const handler = async (m, { conn, text, command, args, usedPrefix }) => {
-    const idioma = global.db.data.users[m.sender].language || 'es'; // تحديد لغة المستخدم، إفتراضياً الإسبانية
+    const idioma = global.db.data.users[m.sender].language || 'ar'; // تحديد لغة المستخدم، إفتراضياً العربية
     const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
     const tradutor = _translate.plugins.fun_simi;
 
-  if (!text) throw `${tradutor.texto1} ${usedPrefix + command} مرحبا ميكو*`;
+    if (!text) throw `${tradutor.texto1} ${usedPrefix + command} مرحبا ميكو*`;
     try {
         const resSimi = await simitalk(text);
         conn.sendMessage(m.chat, { text: resSimi.resultado.simsimi }, { quoted: m });
@@ -23,12 +23,12 @@ handler.command = /^((سيمي|ميكو|بوت|اليكسا|كورتانا))$/i;
 
 export default handler;
 
-async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "es") {
+async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "ar") {
     if (!ask) return { status: false, resultado: { msg: "يجب إدخال نص للتحدث مع ميكو." }};
     try {
         const response1 = await axios.get(`https://delirios-api-delta.vercel.app/tools/simi?text=${encodeURIComponent(ask)}`);
         const trad1 = await translate(`${response1.data.data.message}`, {to: language, autoCorrect: true});
-        if (trad1.text == 'indefinida' || response1 == '' || !response1.data) trad1 = XD; // تعامل مع حالات خاصة للردود غير المتوقعة
+        if (trad1.text == 'indefينida' || response1 == '' || !response1.data) trad1 = XD; // تعامل مع حالات خاصة للردود غير المتوقعة
         return { status: true, resultado: { simsimi: trad1.text }};        
     } catch {
         try {
